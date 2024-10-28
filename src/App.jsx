1719fbb645bfdc6336fa9b0bd001133265197815
pkcbcse <pradipkcb.cse@gmail.com>
2024-10-28 15:31:25 +0600

    1st commit-basic project setup & navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import DaisyNav from './components/DaisyNav/DaisyNav';
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className="text-7xl bg-rose-500">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
